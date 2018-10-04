210a41c85049e6de32457a3e4734db8c3165a3aa
Mary Jack <maryjackinthebox@gmail.com>
2018-10-04 05:27:44 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,11 @@
 ActiveRecord::Schema.define(version: 20181004052601) do
 
   create_table "coupons", force: :cascade do |t|
+    t.string   "coupon_code"
+    t.string   "store"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
   end
 
 end
